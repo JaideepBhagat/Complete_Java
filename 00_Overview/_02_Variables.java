1c8827ba335d213f75dc21c8052080d534288a22
Jaideep <jaideep.bhagat@yahoo.in>
2024-07-16 10:30:14 +0530

    This is the second example in Java 	To illustrate the use of variables

---
+++ 00_Overview/_02_Variables.java
@@ -0,0 +1,17 @@
+/*
+    This is the second example in Java
+	To illustrate the use of variables
+*/
+class _02_Variables {
+	public static void main(String[] args){
+		int num; //this declares a variable called num
+		num = 100; // this assigns a value of 100 to the variable num
+		
+		System.out.println("The value of num is: " + num);
+		
+		num = num * 2;
+		
+		System.out.print("The value of num * 2 is: ");
+		System.out.println(num);
+	}
+}
