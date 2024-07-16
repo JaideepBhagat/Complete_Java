912ddb0dfc42657e377e64874a0b5e4d7c06ea8e
Jaideep <jaideep.bhagat@yahoo.in>
2024-07-16 10:28:55 +0530

    This is a simple Java Program.     And this is our First Example

---
+++ 00_Overview/_01_Welcome.java
@@ -0,0 +1,10 @@
+/*
+    This is a simple Java Program.
+    And this is our First Example 
+*/
+public class _01_Welcome {
+    //Your program begins with a call to main method
+    public static void main(String[] args) {
+        System.out.println("Welcome. This is the First Program in Java");
+    }
+}
