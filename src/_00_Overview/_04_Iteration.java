21c940c58274b7555f57ba5996ae9d7692194d4c
Jaideep <jaideep.bhagat@yahoo.in>
2024-07-16 10:38:39 +0530

    This example illustrates the use of for loop

---
+++ src/_00_Overview/_04_Iteration.java
@@ -0,0 +1,11 @@
+package _00_Overview;
+
+/*
+    This example illustrates the use of for loop
+*/
+public class _04_Iteration {
+    public static void main(String[] args){
+        int x;
+        for(x = 0; x < 10; x =x+1) System.out.println("x = " + x);
+    }
+}
