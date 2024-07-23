f19c40b55e33876706fa7d0b5a41aa4c53a830c1
JaideepBhagat <jaideep.bhagat@yahoo.in>
2024-07-23 15:53:06 +0530

    This example demonstrates the use of two-dimensional arrays * It prints the multiplication table from 1 to 10

--- src/_01_DataTypes_Variables_Arrays/_06_Arrays.java
+++ src/_01_DataTypes_Variables_Arrays/_06_Arrays.java
@@ -1,10 +1,10 @@
 package _01_DataTypes_Variables_Arrays;
-
+/*This example demonstrates the use of arrays*/
 public class _06_Arrays {
     public static void main(String[] args){
         String[] week; // Declaration
         week = new String[7]; // Allocation
-        week[0] = "Sun";
+        week[0] = "Sun"; // Initialization
         week[1] = "Mon";
         week[2] = "Tue";
         week[3] = "Wed";
@@ -14,7 +14,7 @@ public class _06_Arrays {
 
         System.out.println("Forth day in the week is " + week[3]);
 
-        int[] month_days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int[] month_days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; // Initialization of array along with declaration
         System.out.println("August has " + month_days[7] + " days in a month");
     }
 }
